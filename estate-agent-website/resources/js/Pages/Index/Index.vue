a60c59d0d9a5e878787723b32005eeae49f35372
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-03-28 23:55:26 +0100

    First pages and routes

---
+++ estate-agent-website/resources/js/Pages/Index/Index.vue
@@ -0,0 +1,11 @@
+<template> 
+
+<div>Index</div>
+<Link href="hello"> Show Page </Link>
+
+</template>
+
+<script setup>
+import {Link} from "@inertiajs/vue3"
+
+</script>
---
+++ estate-agent-website/resources/js/Pages/Index/Show.vue
@@ -0,0 +1,11 @@
+<template> 
+
+    <div>Index</div>
+    <Link href="/"> Main Page </Link>
+  
+  </template>
+  
+    <script setup>
+    import {Link} from "@inertiajs/vue3"
+  
+  </script>
