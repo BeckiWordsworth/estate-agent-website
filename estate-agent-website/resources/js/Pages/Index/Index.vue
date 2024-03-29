144823200971d52b605177deafe31e14f17d0386
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-03-29 22:18:47 +0100

    Main layout setup

--- estate-agent-website/resources/js/Pages/Index/Index.vue
+++ estate-agent-website/resources/js/Pages/Index/Index.vue
@@ -1,16 +1,14 @@
-<template> 
-
-<div>Index</div>
-<Link href="hello"> Show Page </Link>
-<div>
-  The message is {{ message }}
-</div>
-
+<template>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="hello"> Show Page </Link>
+        <div>The message is {{ message }}</div>
+    </MainLayout>
 </template>
 
 <script setup>
-import {Link} from "@inertiajs/vue3"
+import { Link } from "@inertiajs/vue3";
+import MainLayout from "../../Layouts/MainLayout.vue";
 
-defineProps({message: String})
-
+defineProps({ message: String });
 </script>
